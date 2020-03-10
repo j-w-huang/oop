b3ed51cef91c8726584d313d39b3c20f70c744b5
Jianwei.Huang <996813925@qq.com>
2020-03-11 01:20:13 +0800

    lesson maven simple example...

---
+++ imut-oo-ch01/src/main/java/edu/imut/oo/ch01/GetHello.java
@@ -0,0 +1,19 @@
+package edu.imut.oo.ch01;
+
+/**
+ * 获取HelloWord的类。
+ * 
+ * @author 黄老师
+ *
+ */
+public class GetHello {
+
+  /**
+   * 获取HelloWord字符串
+   * 
+   * @return HelloWord字符串
+   */
+  public String getHello() {
+    return "Hello Word !!!";
+  }
+}
