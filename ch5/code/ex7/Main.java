78800cea0387ad06d422b0bd2a5ce2560c0556a4
Jianwei.Huang <996813925@qq.com>
2020-04-27 17:37:48 +0800

    add ex7 code.

---
+++ ch5/code/ex7/Main.java
@@ -0,0 +1,46 @@
+package com.imut.iot.ex7;
+
+public class Main {
+    public static void divByZero() throws ArithmeticException {
+    	    try {
+			int i = 0;
+			// 编译能过，说明不是错误，但是会产生异常
+			int j = 1 / i;
+			
+		} catch(ArithmeticException e) {
+			throw e;
+		}
+    }
+    
+    /**
+     * 计算两个数之和，当任意一个数超出范围(本例为10～20)时，抛出自己的异常。
+     * @param a,b
+     * @throws NumberRangeException
+     */
+    public static int testNumberRangeExpception(int a, int b) 
+    		throws NumberRangeException {
+    		if(a>20 || a<10) {
+    			throw new NumberRangeException("arg: a is not in 10~20");
+    		}
+    		
+    		if(b>20 || b<10) {
+    			throw new NumberRangeException("arg: a is not in 10~20");
+    		}
+    		
+    		return a+b;
+    }
+    
+	public static void main(String[] args) {
+		
+		try {
+			//divByZero();
+			testNumberRangeExpception(9,12);
+		} catch (NumberRangeException e) {
+			//终止执行，线程的话，线程会挂掉
+			e.printStackTrace();
+		} catch (ArithmeticException e) {
+			//终止执行，线程的话，线程会挂掉
+			e.printStackTrace();
+		}
+	}
+}
---
+++ ch5/code/ex7/NumberRangeException.java
@@ -0,0 +1,15 @@
+package com.imut.iot.ex7;
+
+public class NumberRangeException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public NumberRangeException(String message) {
+		super(message);
+		
+	}
+
+}
